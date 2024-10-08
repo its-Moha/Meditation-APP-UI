7f2f35e46d336d3de04609a7741f5935ce2a3df3
mohamedamin <funstars008@gmail.com>
2024-10-08 13:52:56 +0300

    Building a Meditation App UI

--- app/src/main/java/com/example/meditationappui/ui/theme/Type.kt
+++ app/src/main/java/com/example/meditationappui/ui/theme/Type.kt
@@ -1,6 +1,5 @@
 package com.example.meditationappui.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
