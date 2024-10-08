c6fdfa5517f151bd7b0c2526a01cae84a8e889cf
mohamedamin <funstars008@gmail.com>
2024-10-08 13:51:53 +0300

    Building a Meditation App UI

---
+++ app/src/main/java/com/example/meditationappui/ui/theme/Theme.kt
@@ -0,0 +1,21 @@
+package com.example.meditationappui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun MeditationAppUITheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+
+    MaterialTheme(
+        shapes = shape,
+        typography = Typography,
+        content = content
+    )
+}
